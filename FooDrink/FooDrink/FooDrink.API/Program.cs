7194be9c30db53edb49018ca54d3d61c2c3fecab
ADMIN <ADMIN@TQ23>
2024-05-14 08:18:57 +0700

    update user

--- FooDrink/FooDrink/FooDrink.API/Program.cs
+++ FooDrink/FooDrink/FooDrink.API/Program.cs
@@ -1,33 +1,46 @@
 ﻿using FooDrink.Infrastructure;
-using FooDrink.API;
+using Microsoft.OpenApi.Models;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FooDrink API", Version = "v1" });
+});
 
 // Configure strongly typed settings object
-
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureServiceManager();
 
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     _ = app.UseSwagger();
-    _ = app.UseSwaggerUI();
+    _ = app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FooDrink API v1");
+        c.RoutePrefix = string.Empty; // Set Swagger UI at root
+    });
 }
 
+// Redirect root to Swagger UI
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger");
+        return;
+    }
+
+    await next();
+});
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
